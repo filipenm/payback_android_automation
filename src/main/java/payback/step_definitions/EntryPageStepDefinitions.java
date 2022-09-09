fd75eccb37f171c83b6a010e0d4ea314b20bcbaa
filipenm <filipenm@fuib.com>
2022-09-09 15:54:05 +0200

    Made locator private

--- src/main/java/payback/step_definitions/EntryPageStepDefinitions.java
+++ src/main/java/payback/step_definitions/EntryPageStepDefinitions.java
@@ -9,7 +9,7 @@ public class EntryPageStepDefinitions {
 
     protected ApplicationManager app = ApplicationManager.get();
 
-    private By login = By.id("welcome_loginbutton");
+    private final By login = By.id("welcome_loginbutton");
 
     @Given("I am at the Login page")
     public void i_am_at_the_login_page() {
